85532e206969d3a2d4ca80e17c8c847f328ddf37
MinaLuo <619696434@qq.com>
2021-10-31 23:09:39 +0800

    20211031

---
+++ vue_test/22_插槽/2_src_具名插槽/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <Category title="美食">
+      <img
+        slot="center"
+        src="http://img.mp.itc.cn/upload/20170705/cff8475db93a49ecbc5db18fc14c45f2_th.jpg"
+        alt=""
+      />
+      <a slot="footer" href="http://www.baidu.com">更多美食</a>
+    </Category>
+    <Category title="游戏">
+      <ul slot="center">
+        <li v-for="(g, index) in games" :key="index">{{ g }}</li>
+      </ul>
+      <div class="foot" slot="footer">
+        <a href="http://www.baidu.com">单机游戏</a>
+        <a href="http://www.baidu.com">网络游戏</a>
+      </div>
+    </Category>
+    <Category title="电影">
+      <video
+        slot="center"
+        controls
+        src="https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
+      ></video>
+      <template v-slot:footer>
+        <div class="foot">
+          <a href="http://www.baidu.com">经典</a>
+          <a href="http://www.baidu.com">热门</a>
+          <a href="http://www.baidu.com">推荐</a>
+        </div>
+        <h4>欢迎前来观影</h4>
+      </template>
+    </Category>
+  </div>
+</template>
+
+<script>
+import Category from "./components/Category.vue";
+export default {
+  name: "App",
+  components: { Category },
+  data() {
+    return {
+      foods: ["火锅", "烧烤", "小龙虾", "牛排"],
+      games: ["红色警戒", "穿越火箭", "劲舞团", "超级玛丽"],
+      films: ["《教父》", "《拆弹专家》", "《你好，李焕英》", "《尚硅谷》"],
+    };
+  },
+};
+</script>
+
+<style>
+.container,
+.foot {
+  display: flex;
+  justify-content: space-around;
+}
+video {
+  width: 100%;
+}
+h4 {
+  text-align: center;
+}
+</style>
