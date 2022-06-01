cea80132fb4d37d081c694616aae80870405531a
MinaLuo <619696434@qq.com>
2022-06-01 10:51:54 +0800

    20220601

---
+++ vuex-test/src/App_Tabs_ref.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+   <div v-if='activeStep ===0'>
+    第一步
+   </div>
+  <div v-if='activeStep ===1'>
+    <el-tabs v-model="activeName" @tab-click="handleClick" ref="tabsEl">
+        <el-tab-pane v-if="isShow" label="用户管理" name="first">用户管理</el-tab-pane>
+        <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
+        <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
+        <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+    </el-tabs>
+  </div>
+  <button style="margin-top: 12px;" @click="next">下一步</button>
+</div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        activeStep:0,
+        activeName: 0,
+        isShow: false,
+      };
+    },
+    methods: {
+    // async 
+    next(){
+    //    await this.$nextTick()
+       this.activeStep = 1
+       setTimeout(() => {
+            console.log(this.$refs.tabsEl,'tabsEl')
+            this.activeName = this.$refs.tabsEl.panes[0].name;
+       },2000)
+      
+     },
+     async handleClick(tab, event) {
+        console.log(tab, event);
+      }
+    },
+    async beforeCreate() {
+        // await this.$nextTick()
+        console.log('beforeCreate',this.$refs.tabsEl)
+    },
+  };
+</script>
--- vuex-test/src/main.js
+++ vuex-test/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import App from './App_Tabs_ref.vue'
 import store from './store';
 import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
